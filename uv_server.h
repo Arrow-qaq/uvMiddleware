cac2dee9ab61427d89ef35b68149122d8878be52
Arrow-qaq <57163797+Arrow-qaq@users.noreply.github.com>
2023-03-29 08:49:03 +0800

    Update uv_server.h

--- uv_server.h
+++ uv_server.h
@@ -1,61 +1,37 @@
-/*
- * @Descripttion: this is test
- * @version:
- * @Author: Arrow
- * @Date: 2023-03-29 08:43:37
- * @LastEditors: Arrow
- * @LastEditTime: 2023-03-29 08:43:58
- */
-
 #include <uv.h>
 #include <iostream>
 #include <memory>
 
-class TcpClient {
+class TcpServer {
 public:
-    TcpClient(uv_loop_t *loop)
+    TcpServer(uv_loop_t *loop)
     {
-        uv_tcp_init(loop, &client_handle_);
-        connect_req_ = std::make_unique< uv_connect_t >();
-        read_req_    = std::make_unique< uv_read_t >();
+        uv_tcp_init(loop, &server_handle_);
+        read_req_ = std::make_unique< uv_read_t >();
     }
 
-    ~TcpClient() { uv_close((uv_handle_t *)&client_handle_, nullptr); }
+    ~TcpServer() { uv_close((uv_handle_t *)&server_handle_, nullptr); }
 
-    void connect(const std::string &host, int port, std::function< void() > on_connect)
+    void listen(int port, std::function< void() > on_listen)
     {
-        sockaddr_in addr = uv_ip4_addr(host.c_str(), port);
+        sockaddr_in addr = uv_ip4_addr("0.0.0.0", port);
 
-        uv_tcp_connect(connect_req_.get(), &client_handle_, (const struct sockaddr *)&addr,
-            [on_connect](uv_connect_t *req, int status) {
+        uv_tcp_bind(&server_handle_, (const struct sockaddr *)&addr, 0);
+        uv_listen(
+            (uv_stream_t *)&server_handle_, 128, [on_listen](uv_stream_t *server, int status) {
                 if (status == -1) {
-                    std::cerr << "failed to connect" << std::endl;
+                    std::cerr << "failed to listen" << std::endl;
                 } else {
-                    std::cout << "connected successfully" << std::endl;
-                    on_connect();
+                    std::cout << "listening on port " << server->port << std::endl;
+                    on_listen();
                 }
             });
     }
 
-    void send(const std::string &message, std::function< void() > on_send)
+    void receive(uv_stream_t *client, std::function< void(const char *, ssize_t) > on_receive)
     {
-        auto write_req = std::make_unique< uv_write_t >();
-        auto buf       = uv_buf_init((char *)message.c_str(), message.length());
-
-        uv_write(write_req.get(), (uv_stream_t *)&client_handle_, &buf, 1,
-            [on_send](uv_write_t *req, int status) {
-                if (status == -1) {
-                    std::cerr << "failed to send message" << std::endl;
-                } else {
-                    std::cout << "message sent successfully" << std::endl;
-                    on_send();
-                }
-            });
-    }
-
-    void receive(std::function< void(const char *, ssize_t) > on_receive)
-    {
-        uv_read_start((uv_stream_t *)&client_handle_,
+        uv_read_start(
+            client,
             [](uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
                 buf->base = new char[suggested_size];
                 buf->len  = suggested_size;
@@ -71,8 +47,22 @@ public:
             });
     }
 
+    void send(uv_stream_t *client, const std::string &message, std::function< void() > on_send)
+    {
+        auto write_req = std::make_unique< uv_write_t >();
+        auto buf       = uv_buf_init((char *)message.c_str(), message.length());
+
+        uv_write(write_req.get(), client, &buf, 1, [on_send](uv_write_t *req, int status) {
+            if (status == -1) {
+                std::cerr << "failed to send message" << std::endl;
+            } else {
+                std::cout << "message sent successfully" << std::endl;
+                on_send();
+            }
+        });
+    }
+
 private:
-    uv_tcp_t                        client_handle_;
-    std::unique_ptr< uv_connect_t > connect_req_;
-    std::unique_ptr< uv_read_t >    read_req_;
+    uv_tcp_t                     server_handle_;
+    std::unique_ptr< uv_read_t > read_req_;
 };
